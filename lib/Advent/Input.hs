ccde0031d4c2afc7048a5db2da174ca784a91d61
Melanie Brown <brown.m@pm.me>
2021-11-24 09:04:43 -0500

    autofetch input if missing

--- lib/Advent/Input.hs
+++ lib/Advent/Input.hs
@@ -6,9 +6,17 @@ import System.Directory
 import System.FilePath ((<.>), (</>))
 
 input :: Natural -> Natural -> Text
-input (show -> year) (show -> day) = unsafePerformIO $ do
+input y@(show -> year) d@(show -> day) = unsafePerformIO $ do
   let inputfile = "./input" </> year </> day <.> "txt"
-      aocopts = AoCOpts {_aSessionKey = "53616c7465645f5fc17036fcdfe1e13dbbc124e1151a6fa12c2af184ebaf0bc272988346ece9d9f382a1b142d580dbb2"
-      }
+      aocopts =
+        AoCOpts
+          { _aSessionKey = "53616c7465645f5fc17036fcdfe1e13dbbc124e1151a6fa12c2af184ebaf0bc272988346ece9d9f382a1b142d580dbb2",
+            _aYear = fromIntegral y,
+            _aCache = Nothing,
+            _aForce = False,
+            _aThrottle = 10 ^ (6 :: Int)
+          }
   ifM (doesFileExist inputfile) (readFileText inputfile) $ do
-    pure ()
+    t <- runAoC_ aocopts (AoCInput . mkDay_ $ fromIntegral d)
+    writeFileText inputfile t
+    pure t
