9da620c2eef6d700d3904c580f08c8dead739bcf
Melanie Brown <brown.m@pm.me>
2021-12-01 00:08:55 -0500

    2021 day 1

--- app/Advent21/Day01.hs
+++ app/Advent21/Day01.hs
@@ -1,1 +1,12 @@
 module Day01 where
+
+in01 :: [Integer]
+in01 = parse number <$> lines (input 2021 1)
+
+part1 :: Natural
+part1 = withNonEmpty 0 (count (\(a :| [b]) -> a < b) . init) $ pairsTied in01
+
+part2 :: Natural
+part2 =
+  let threes = zipWith3 (\a b c -> a + b + c) in01 (drop 1 in01) (drop 2 in01)
+   in withNonEmpty 0 (count (\(a :| [b]) -> a < b) . init) $ pairsTied threes
